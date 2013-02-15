33c5b2039b7e1f482402c9a8ac4eedd7d5ac9748
Daniel <sidagraf@gmail.com>
2013-02-15 12:15:05 +0200

    Revert "test commit"
    This reverts commit c8602354b9fb5ef1f64a8954640d633d44b9456c.

--- src/GeneticJVM/core.clj
+++ src/GeneticJVM/core.clj
@@ -4,11 +4,3 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
-
-(defn welcome01234
-  [x,y]
-  (println x y))
-
-(defn welcome
-  [x]
-  (welcome01234 "Welcome To The Clojure Age, " x))
