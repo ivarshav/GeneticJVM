c172dea155aa55f54871f9ef6a3410a9b36528dd
Daniel <sidagraf@gmail.com>
2013-02-18 02:32:35 +0200

    Init Person and Population
    PLEASE READ NOTE IN THE FILE
    - Added initilizing of person and population

--- src/GeneticJVM/core.clj
+++ src/GeneticJVM/core.clj
@@ -1,22 +1,39 @@
 (ns GeneticJVM.core)
 
-;Fooling around
-(defn welcome
-  [x]
-  (println "Welcome To The Clojure Age, " x))
+;Daniel's Update 18/02
+;NOTE: I used different approach: instead of using a vector and loop i used recursive and cond and by so recieved the needed action - CONJing person in population and char in persons!!
 
-;Real job:
+(def abc ["a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "!"])
 
-(def abc ["a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" " " "!"])
+;is-person-space? returns true when i equals to 1 or 6 which are the matching indices of the spaces in "I Love Clojure"
+(defn is-person-space? [i] (or (= i 1) (= i 6)))
 
-(def PopulationSize 30)
-
-;Create Person
-;Population loop NOT TESTED! PErson loop works!
-;Need to add CONJ to Population map anf create it
-(dotimes [n PopulationSize]
-  (
-		(def Person [])
-		(dotimes [n 15](def person (conj person (rand-nth abc))))
+(defn getPersonChar
+  [i]
+  (cond
+  (is-person-space? i) " "
+  :else (rand-nth abc) 
   )
 )
+
+(def PersonLength 15)
+(def PopulationSize 30)
+
+;_InitPerson returns a random initial person vector
+(defn _InitPerson [i person]
+  (cond
+    (= i PersonLength) person
+    :else   (cons (getPersonChar i) (_InitPerson (+ i 1) person))
+    )
+  )
+
+;_InitPopulation returns a random intial population vector
+(defn _InitPopulation [i pop]
+  (cond
+    (= i PopulationSize) pop
+    :else (cons {(_InitPerson 0 []) -1} (_InitPopulation (+ i 1) pop))
+    ;we evaluate each person with -1 meanwhile... later it will have its fitness
+    )
+  )
+
+(def InitPopulation (_InitPopulation 0 {}))
